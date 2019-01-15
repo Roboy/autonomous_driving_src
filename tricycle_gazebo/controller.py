b9f6145543cb938bceaf6209b9c2ac9df25c1cb5
Alex Melkonyan <sasha.melkonyan@gmail.com>
2019-01-15 16:29:07 +0100

    Intermediate commit, nothing work

--- tricycle_gazebo/controller.py
+++ tricycle_gazebo/controller.py
@@ -6,9 +6,13 @@ navigation/gazebo/tricycle.urdf
 """
 import rospy
 from std_msgs.msg import Float64MultiArray, Float64
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PoseWithCovarianceStamped
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from gazebo_msgs.msg import ModelState
 import random
 
+#TODO: replace with parameter
+MODEL_NAME = 'tricycle'
 
 EPS = 1e-04
 
@@ -17,7 +21,9 @@ class TricycleControlGazebo:
     def __init__(self):
         pass
 
+
     def start(self):
+        set_robot_pos = rospy.ServiceProxy('/gazebo/set_model_state', ModelState)
         rospy.init_node('tricycle_control_gazebo', log_level=rospy.DEBUG)
         rear_wheels_pub = rospy.Publisher(
             '/tricycle_rear_wheels_controller/command', Float64MultiArray,
@@ -25,6 +31,17 @@ class TricycleControlGazebo:
         front_wheel_pub = rospy.Publisher(
             '/tricycle_front_wheel_controller/command', Float64,
             queue_size=10)
+
+        def handle_initialpose(pose):
+            rospy.loginfo('Resetting poisition of the %s', MODEL_NAME)
+            pos = pose.pose.pose
+            robot_state = ModelState()
+            robot_state.model_name = MODEL_NAME
+            robot_state.pose = pos
+            success, status_msg = set_robot_pos(robot_state)
+            if not success:
+                rospy.logerror('Position change failed. Error:\n %s', status_msg)
+
 
         def handle_velocity(twist):
             vx, vy = twist.linear.x, twist.linear.y
@@ -37,6 +54,8 @@ class TricycleControlGazebo:
             rear_wheels_pub.publish(lin_vel)
 
         rospy.Subscriber('cmd_vel', Twist, handle_velocity, queue_size=1)
+        #rospy.Subscriber('initialpose', PoseWithCovarianceStamped,
+        #                 handle_initialpose, queue_size=1)
         rospy.spin()
 
 if __name__ == '__main__':
