f8dff6753b03ad8f72c2176d17eb80aa3ea12a77
Chris <34374252+christqoh@users.noreply.github.com>
2019-01-22 12:01:38 +0100

    updated realsense stuff

--- package_requirements.sh
+++ package_requirements.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
+add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main" -u
+rm -f /etc/apt/sources.list.d/realsense-public.list
 apt-get update  # To get the latest package lists
 apt-get install ros-kinetic-map-server ros-kinetic-sick-scan python-wstool python-rosdep ninja-build ros-kinetic-abseil-cpp ros-kinetic-geometry2 libarmadillo-dev ros-kinetic-moveit-msgs librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg ros-kinetic-cv-bridge -y
 #etc.
