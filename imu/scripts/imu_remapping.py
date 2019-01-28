a37a40e67227d51c5873ba21ba8513bfd0bee332
Chris <34374252+christqoh@users.noreply.github.com>
2019-01-28 18:00:02 +0100

    further refindements

--- imu/scripts/imu_remapping.py
+++ imu/scripts/imu_remapping.py
@@ -10,35 +10,28 @@ from sensor_msgs.msg import Imu
 from sbg_driver.msg import SbgImuData
 
 class Converter:
-    def __init__(self):
-        rospy.init_node('imu_data_converter', anonymous=False)
-    	self.pub = rospy.Publisher('imu', Imu, queue_size=10)
-    	rospy.Subscriber('imu_data', SbgImuData, self.callback)
-    	rospy.spin()
+	def __init__(self):
 
-    def callback(self, msg):
-    	ROS_IMU_msg = self.mapping(msg)
+		# set up ROS node
+		rospy.init_node('imu_data_converter', anonymous=False)
+		self.pub = rospy.Publisher('imu', Imu, queue_size=10)
+		rospy.Subscriber('imu_data', SbgImuData, self.mapping)
+		rospy.spin()
 
-    def mapping(self, SBG_IMU_msg):
-            ROS_IMU_msg = Imu()
-	    ROS_IMU_msg.linear_acceleration = SBG_IMU_msg.accel
-	    ROS_IMU_msg.angular_velocity = SBG_IMU_msg.gyro
+	def mapping(self, SBG_IMU_msg):
 
-	    print("SBG Acceleration: ", SBG_IMU_msg.accel)
-	    print("ROS Acceleration: ", ROS_IMU_msg.linear_acceleration)
-	    print("SBG Gyro: ", SBG_IMU_msg.gyro)
-	    print("ROS Gyro: ", ROS_IMU_msg.angular_velocity)
+		# remap data from SBG_IMU_msg to ROS_IMU_msg and publish
+		ROS_IMU_msg = Imu()
+		ROS_IMU_msg.linear_acceleration = SBG_IMU_msg.accel
+		ROS_IMU_msg.angular_velocity = SBG_IMU_msg.gyro
 
-	    self.pub.publish(ROS_IMU_msg)
+		self.pub.publish(ROS_IMU_msg)
 
 if __name__ == '__main__':
-    try:
-	Converter()
-    except rospy.ROSInterruptException:
-        pass
-
-
-
+	try:
+		Converter()
+	except rospy.ROSInterruptException:
+		pass
 
 
 
