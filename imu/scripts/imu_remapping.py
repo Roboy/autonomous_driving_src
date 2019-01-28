cf87cb19a8fccc6d5fae009e17ad1802c9438ff1
Chris <34374252+christqoh@users.noreply.github.com>
2019-01-28 13:37:05 +0100

    header block

--- imu/scripts/imu_remapping.py
+++ imu/scripts/imu_remapping.py
@@ -1,40 +1,9 @@
 #!/usr/bin/env python
 # Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
 
-## Simple talker demo that published std_msgs/Strings messages
-## to the 'chatter' topic
+## Simple IMU signal conversion node taking input from sbg_driver
+## custom message type and publishing linear acceleration to 
+## ROS /imu topic in standard IMU message format
 
 import rospy
 from sensor_msgs.msg import Imu
@@ -44,7 +13,11 @@ def callback(msg):
     try:
         print("Acceleration: ", msg.accel)
     except:
+        pass
+    try:
         print("Acceleration: ", msg.linear_acceleration)
+    except:
+        pass
 
     #print("Gyro: ", msg.gyro)
 #    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
@@ -67,7 +40,7 @@ def IMU_publisher(ROS_IMU_msg, pub):
 def IMU_Converter():
 
     # init node, subscriber, publisher and keep them running
-    rospy.init_node('imu_conversion', anonymous=False)
+    rospy.init_node('imu_data_converter', anonymous=False)
     rospy.Subscriber('imu_data', SbgImuData, callback)
     pub = rospy.Publisher('imu', Imu, queue_size=10)
     rospy.spin()
@@ -77,17 +50,6 @@ def IMU_Converter():
 
     # Publish converted signal
     IMU_publisher(ROS_IMU_msg, pub)
-
-
-#def talker():
-#    pub = rospy.Publisher('chatter', String, queue_size=10)
-    #rospy.init_node('talker', anonymous=True)
-#    rate = rospy.Rate(10) # 10hz
-#    while not rospy.is_shutdown():
-#        hello_str = "hello world %s" % rospy.get_time()
-#        rospy.loginfo(hello_str)
-#        pub.publish(hello_str)
-#        rate.sleep()
 
 if __name__ == '__main__':
     try:
