5b966d7aeca98fc64e392191785950aa7c62548a
Chris <34374252+christqoh@users.noreply.github.com>
2019-01-28 13:21:16 +0100

    first version of code

--- imu/scripts/imu_remapping.py
+++ imu/scripts/imu_remapping.py
@@ -37,23 +37,46 @@
 ## to the 'chatter' topic
 
 import rospy
-from std_msgs.msg import Imu
-from sbg_driver import SbgImuData
+from sensor_msgs.msg import Imu
+from sbg_driver.msg import SbgImuData
+
+def callback(msg):
+    try:
+        print("Acceleration: ", msg.accel)
+    except:
+        print("Acceleration: ", msg.linear_acceleration)
+
+    #print("Gyro: ", msg.gyro)
+#    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+
+def mapping(SBG_IMU_msg):
+    
+    ROS_IMU_msg.linear_acceleration = SBG_IMU_msg.accel
+    #ROS_IMU_msg.acceleration = SBG_IMU_msg.gyro
+    callback(ROS_IMU_msg)
+
+    return ROS_IMU_msg
+
+def IMU_publisher(ROS_IMU_msg, pub):
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        rospy.loginfo(ROS_IMU_msg)
+        pub.publish(ROS_IMU_msg)
+        rate.sleep()
 
 def IMU_Converter():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
+    # init node, subscriber, publisher and keep them running
     rospy.init_node('imu_conversion', anonymous=False)
-
-    rospy.Subscriber('SbgImuData', String, callback)
-    pub = rospy.Publisher('imu', String, queue_size=10)
-
-    # spin() simply keeps python from exiting until this node is stopped
+    rospy.Subscriber('imu_data', SbgImuData, callback)
+    pub = rospy.Publisher('imu', Imu, queue_size=10)
     rospy.spin()
+
+    # map from SBG driver to ROS message
+    ROS_IMU_msg = mapping(SBG_IMU_msg)
+
+    # Publish converted signal
+    IMU_publisher(ROS_IMU_msg, pub)
 
 
 #def talker():
@@ -72,12 +95,9 @@ if __name__ == '__main__':
     except rospy.ROSInterruptException:
         pass
 
-#def callback(data):
-#    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
 
 
-#if __name__ == '__main__':
-#    listener()
+
 
 
 
