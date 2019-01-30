e60101129c12b635d6af09318f39dc4fc87a7528
Chris <34374252+christqoh@users.noreply.github.com>
2019-01-30 19:06:08 +0100

    changing signs of imu data

--- roboy_ad/scripts/imu_remapping.py
+++ roboy_ad/scripts/imu_remapping.py
@@ -25,11 +25,13 @@ class Converter:
 		ROS_IMU_msg.header.frame_id = 'imu'
 		ROS_IMU_msg.header.seq = SBG_IMU_msg.header.seq
 		ROS_IMU_msg.header.stamp = SBG_IMU_msg.header.stamp
-		#ROS_IMU_msg.time_stamp = SBG_IMU_msg.time_stamp
 
-		ROS_IMU_msg.linear_acceleration = SBG_IMU_msg.accel
-		ROS_IMU_msg.angular_velocity = SBG_IMU_msg.gyro
-
+		ROS_IMU_msg.linear_acceleration.x = SBG_IMU_msg.accel.x
+		ROS_IMU_msg.linear_acceleration.y = - SBG_IMU_msg.accel.y
+		ROS_IMU_msg.linear_acceleration.z = - SBG_IMU_msg.accel.z
+		ROS_IMU_msg.angular_velocity.x = SBG_IMU_msg.gyro.x
+		ROS_IMU_msg.angular_velocity.y = - SBG_IMU_msg.gyro.y
+		ROS_IMU_msg.angular_velocity.z = - SBG_IMU_msg.gyro.z
 
 		self.pub.publish(ROS_IMU_msg)
 
