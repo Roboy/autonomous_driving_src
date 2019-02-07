0fdcce27c0ba52ddc1da1c1acf3e3d6d0110e316
Chris <34374252+christqoh@users.noreply.github.com>
2019-02-07 16:02:54 +0100

    moving files and cleaning up

--- roboy_ad/imu/imu_remapping.py
+++ roboy_ad/imu/imu_remapping.py
@@ -27,10 +27,10 @@ class Converter:
 		ROS_IMU_msg.header.stamp = SBG_IMU_msg.header.stamp
 
 		ROS_IMU_msg.linear_acceleration.x = - SBG_IMU_msg.accel.x
-		ROS_IMU_msg.linear_acceleration.y = - SBG_IMU_msg.accel.y
+		ROS_IMU_msg.linear_acceleration.y = SBG_IMU_msg.accel.y
 		ROS_IMU_msg.linear_acceleration.z = SBG_IMU_msg.accel.z
 		ROS_IMU_msg.angular_velocity.x = - SBG_IMU_msg.gyro.x
-		ROS_IMU_msg.angular_velocity.y = - SBG_IMU_msg.gyro.y
+		ROS_IMU_msg.angular_velocity.y = SBG_IMU_msg.gyro.y
 		ROS_IMU_msg.angular_velocity.z = SBG_IMU_msg.gyro.z
 
 		ROS_IMU_msg.orientation_covariance[0] = -1
